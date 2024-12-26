c4cc458ad6a6fb97343ed37f577a18ab70a9083f
Suraj Kumar <surajkumar06174@gmail.com>
2024-12-26 22:00:54 +0530

    modified:   test.js

--- test.js
+++ test.js
@@ -22,87 +22,92 @@
 // let currentDate = Date.now();
 // console.log(currentDate);
 
+//------------------------------------------------------------
 
-function sayMyName(){
-    console.log("S");
-    console.log("U");
-    console.log("R");
-    console.log("A");
-    console.log("J");
-}
+// function sayMyName(){
+//     console.log("S");
+//     console.log("U");
+//     console.log("R");
+//     console.log("A");
+//     console.log("J");
+// }
 
 // sayMyName()
+
+//------------------------------------------------------------
 
 // function addTwoNumbers(number1, number2){
 
 //     console.log(number1 + number2);
 // }
 
-function addTwoNumbers(number1, number2){
+// function addTwoNumbers(number1, number2){
 
     // let result = number1 + number2
     // return result
-    return number1 + number2
-}
+//     return number1 + number2
+// }
 
-const result = addTwoNumbers(3, 5)
+// const result = addTwoNumbers(3, 5)
 
 // console.log("Result: ", result);
 
 
-function loginUserMessage(username = "sam"){
-    if(!username){
-        console.log("PLease enter a username");
-        return
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 
 // console.log(loginUserMessage("Suraj"))
 // console.log(loginUserMessage("Suraj"))
 
+//------------------------------------------------------------
 
-function calculateCartPrice(val1, val2, ...num1){
-    return num1
-}
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
 
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 
-const user = {
-    username: "Suraj",
-    prices: 199
-}
+// const user = {
+//     username: "Suraj",
+//     prices: 199
+// }
 
-function handleObject(anyobject){
+// function handleObject(anyobject){
     // console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
-}
+// }
 
 // handleObject(user)
-handleObject({
-    username: "sam",
-    price: 399
-})
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
 
-const myNewArray = [200, 400, 100, 600]
+// const myNewArray = [200, 400, 100, 600]
 
-function returnSecondValue(getArray){
-    return getArray[1]
-}
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
 
 // console.log(returnSecondValue(myNewArray));
 // console.log(returnSecondValue([200, 400, 500, 1000]));
 
+//------------------------------------------------------------
+
 // Immediately Invoked Function Expressions (IIFE)
 
 
-(function chai(){
-    // named IIFE
-    // console.log(`DB CONNECTED`);
-})();
+// (function chai(){
+//     console.log(`DB CONNECTED`);
+// })();
 
-( (name) => {
-    // console.log(`DB CONNECTED TWO ${name}`);
-} )('suraj')
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// } )('suraj')
 
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
@@ -112,7 +117,7 @@ function returnSecondValue(getArray){
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "suraj"})
+// const addTwo = (num1, num2) => ({username: "suraj"})
 // console.log(addTwo(3, 4))
 
 
@@ -146,3 +151,16 @@ const addTwo = (num1, num2) => ({username: "suraj"})
 // })
 
 //------------------------------------------------------------
+
+// Objects in Array
+
+// const myCoding = [
+//     {name: 'java', price: 200},
+//     {name: 'python', price: 400},
+//     {name: 'javascript', price: 500},
+//     {name: 'ruby', price: 100}
+// ]
+
+// myCoding.forEach((item)=>{
+//     console.log(item.name + ' and the price is ' + '= ' + item.price);
+// })
